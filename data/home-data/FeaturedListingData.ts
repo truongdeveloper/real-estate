f1d5f4d29b788fe9182a809b79dd3bd8565a6f2c
truong <vanminhtruong678@gmail.com>
2024-03-23 12:08:44 +0700

    Fix Deloy Vercel

--- data/home-data/FeaturedListingData.ts
+++ data/home-data/FeaturedListingData.ts
@@ -68,6 +68,81 @@ const listingData: typeListRealEstate[] = [
     id: 4,
     maBDS: 3,
     maNguoiDung: 1,
+    tieuDe: "Khu đô thị mới",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-21T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 248888888,
+      trangThai: 0,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 0,
+      phongBep: null,
+      soTang: 2,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_01.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+  {
+    id: 5,
+    maBDS: 3,
+    maNguoiDung: 1,
     tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
     noiDung: `
     - Thanh toán sớm chiết khấu 19,3%.
@@ -92,6 +167,11 @@ const listingData: typeListRealEstate[] = [
       giaThue: 1000000,
       trangThai: 0,
       diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 0,
+      namXayDung: 2,
       kinhDo: 20.9512135,
       viDo: 105.982102,
       loaiBDS: {
@@ -108,19 +188,556 @@ const listingData: typeListRealEstate[] = [
         {
           id: 1,
           maBDS: 3,
-          url: "url",
+          url: "/assets/images/listing/img_02.jpg",
         },
         {
           id: 2,
           maBDS: 3,
-          url: "url2",
+          url: "/assets/images/listing/img_02.jpg",
         },
         {
           id: 3,
           maBDS: 3,
-          url: "url2",
+          url: "/assets/images/listing/img_03.jpg",
         },
       ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+  {
+    id: 6,
+    maBDS: 3,
+    maNguoiDung: 1,
+    tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-09T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 1000000,
+      trangThai: 0,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 2,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+
+  {
+    id: 7,
+    maBDS: 3,
+    maNguoiDung: 1,
+    tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-09T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 1000000,
+      trangThai: 0,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 2,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_04.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+  {
+    id: 8,
+    maBDS: 3,
+    maNguoiDung: 1,
+    tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-09T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 1000000,
+      trangThai: 0,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 2,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_05.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+  {
+    id: 9,
+    maBDS: 3,
+    maNguoiDung: 1,
+    tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-09T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 1000000,
+      trangThai: 0,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 2,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_06.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+  {
+    id: 10,
+    maBDS: 3,
+    maNguoiDung: 1,
+    tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-09T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 1000000,
+      trangThai: 0,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 2,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_07.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+  {
+    id: 11,
+    maBDS: 3,
+    maNguoiDung: 1,
+    tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-09T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 1000000,
+      trangThai: 0,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 0,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_08.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
+    },
+  },
+  {
+    id: 12,
+    maBDS: 3,
+    maNguoiDung: 1,
+    tieuDe: "Hải Âu Đông Nam Vinhomes Ocean Park 2",
+    noiDung: `
+    - Thanh toán sớm chiết khấu 19,3%.
+    - Thanh toán tiến độ chiết khấu 18%.
+    - Ngoài ra có thể chọn phương thức miễn lãi 36 tháng, miễn lãi 24 tháng chiết khấu 8%.`,
+    ngayDangBai: "2024-03-09T17:00:00.000+00:00",
+    ngayHetHan: "2024-10-09T17:00:00.000+00:00",
+    sdt: "0987654321",
+    trangThai: 0,
+    batDongSan: {
+      id: 3,
+      maLoaiBDS: 1,
+      maNguoiThue: null,
+      maViTri: 1,
+      tenBDS: " Vinhomes Ocean Park 2",
+      dienTich: 300,
+      moTa: ` Bể bơi Pha Lê bốn mùa 2000m siêu VIP, bao gồm mái kính trong suốt, Bể bơi được chia làm hai phần: Bể bơi theo tiêu chuẩn olympic dành cho người lớn có diện tích 1000m, Bể bơi trẻ em rộng khoảng 120m và khu vực dành cho cảnh quan và chức năng phụ trợ rộng hơn 700m. Đặc biệt, nguyên vật liệu sử dụng tại Bể bơi đều đạt chuẩn Châu Âu và đều được nhập từ các thương hiệu lớn của các nước như Tây Ban Nha, Pháp, Đức, Anh,...
+    
+      * Căn góc biệt thự đơn lập một trong số căn Vip nhất dự án hiếm hoi trên thị trường.
+      
+      - Lô góc 2 mặt tiền. Diện tích 203m². Xây dựng 4 tầng.`,
+      giaThue: 1000000,
+      trangThai: 1,
+      diaChi: "Khu Vinhome",
+      phongNgu: 2,
+      phongTam: 2,
+      phongBep: 2,
+      soTang: 2,
+      namXayDung: 2,
+      kinhDo: 20.9512135,
+      viDo: 105.982102,
+      loaiBDS: {
+        id: 1,
+        tenLoaiBDS: "Căn hộ chung cư",
+      },
+      viTri: {
+        id: 1,
+        tinhTp: "Hưng Yên",
+        quanHuyen: "Văn Giang",
+        xaPhuong: "Nghĩa Trụ",
+      },
+      hinhAnhList: [
+        {
+          id: 1,
+          maBDS: 3,
+          url: "/assets/images/listing/img_09.jpg",
+        },
+        {
+          id: 2,
+          maBDS: 3,
+          url: "/assets/images/listing/img_02.jpg",
+        },
+        {
+          id: 3,
+          maBDS: 3,
+          url: "/assets/images/listing/img_03.jpg",
+        },
+      ],
+      tienNghi: {
+        id: 3,
+        tuLanh: 1,
+        mayGiat: 1,
+        hoBoi: 0,
+        wifi: 1,
+        baiDoXe: 1,
+        thangMay: 1,
+        vuon: 1,
+        gara: 0,
+      },
     },
   },
 ];
